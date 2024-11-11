d4ab9411dfb98dbe3620d238521baeaba0a2e22c
ffmdn13 <fafasnakes314@gmail.com>
2024-11-11 14:20:16 +0700

    Menambahkan aside dan main pada dashboard-new

--- resources/views/dashboard-new.blade.php
+++ resources/views/dashboard-new.blade.php
@@ -28,7 +28,55 @@
     <title>Dashborad | RAIN</title>
 </head>
 
-<body>
+<body class="d-flex min-vh-100">
+    {{-- dashboard aside start --}}
+    <aside class="aside-nav border-end border-black px-2">
+        <div class="d-flex align-items-center border-bottom border-black">
+            <img class="logo-img" src="{{ asset('storage/2d-logo.png') }}" alt="RAIN Team">
+            <h2 class="logo-title">RAIN</h2>
+        </div>  
+        <div class="aside-list py-4">
+            <div class="border-bottom border-black" style="height: 300px;">
+                <p class="aside-subheading">MENU UTAMA</p>
+                <div class="aside-list-item py-2 px-2 text-white mb-2" onclick=""><i
+                        class="bi bi-house-door me-1"></i> Beranda</div>
+                <div class="aside-list-item py-2 px-2 text-white mb-2" onclick=""><i
+                        class="bi bi-card-list me-1"></i> Daftar Lamaran</div>
+                @if (false)
+                    <div class="aside-list-item py-2 px-2 text-white mb-2" onclick=""><i
+                            class="bi bi-plus-circle me-1"></i> Tambah Lowongan</div>
+                    <div class="aside-list-item py-2 px-2 text-white mb-2" onclick=""><i
+                            class="bi bi-window me-1"></i> Kelola Lowongan</div>
+                    <div class="aside-list-item py-2 px-2 text-white mb-2" onclick=""><i
+                            class="bi bi-person-vcard me-1"></i> Daftar Pelamar</div>
+                @endif
+            </div>
+            <div class="">
+                <p class="aside-subheading">Lainnya</p>
+                <div class="aside-list-item py-2 px-2 text-white mb-2" onclick=""><i
+                        class="bi bi-gear me-1"></i> Pengaturan</div>
+                <div class="aside-list-item py-2 px-2 text-white mb-2" onclick=""><i
+                        class="bi bi-box-arrow-left me-1"></i> Keluar</div>
+            </div>
+        </div>
+    </aside>
+    {{-- dashboard aside end --}}
+
+    {{-- dashboard main start --}}
+    <main class="dashboard-main">
+
+        <div class="dashboard-main-nav border border-black d-flex align-items-center">
+            <div class="d-flex align-items-center justify-content-between">
+                <div class="d-flex align-items-center gap-1">
+                    <img src="" alt="" class="rounded-cricle shadow">
+                    <p>Wasyn Sulaiman Siregar</p>
+                </div>
+            </div>
+        </div>
+
+    </main>
+    {{-- dashboard main end --}}
+
 </body>
 
 </html>
