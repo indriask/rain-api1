532cc1ea1225826d0e6346e7f4640297d9c8328e
ffmdn13 <fafasnakes314@gmail.com>
2024-11-24 11:55:10 +0700

    Deleting blade component

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Blade::component('head', Head::class);
-        Blade::component('test', Test::class);
+        
     }
 }
