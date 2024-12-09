950f31bb1c25f9451d835e37964e861d8f0d92b6
ffmdn13 <fafasnakes314@gmail.com>
2024-12-09 22:53:38 +0700

    Fix merge conflict on SigninController file

--- app/Http/Controllers/SigninController.php
+++ app/Http/Controllers/SigninController.php
@@ -9,28 +9,16 @@ use Illuminate\Support\Facades\Hash;
 
 class SigninController extends Controller
 {
-<<<<<<< HEAD
     /**
      * Method untuk me-render halaman signin mahasiswa dan perusahaan
      */
-=======
->>>>>>> c6efe51c5ae24bc550f217dd1fb39696f1de8917
     public function index()
     {
         return view('signin');
     }
 
-<<<<<<< HEAD
-    /**
-     * Method untuk mem-proses logika signin mahasiswa dan perusahaan
-     */
-    public function signin(Request $request)
-    {
-        dd($request->all());
-=======
     public function validateCredentials(Request $request)
     {
->>>>>>> c6efe51c5ae24bc550f217dd1fb39696f1de8917
         try {
             // Validasi input
             $validated = $request->validate(
@@ -45,33 +33,12 @@ class SigninController extends Controller
                 ]
             );
 
-<<<<<<< HEAD
-            $data = [
-                'email' => $request->email_or_phone,
-                'password' => $request->password
-            ];
-            $response = Http::asForm()->post('http://localhost/RAIN/public/api/signin', $data);
-
-            if ($response->successful()) {
-                // Anda bisa menangani token atau data respons API di sini, misalnya menyimpan token session
-                // Misalnya jika API mengembalikan token JWT, Anda bisa menyimpannya di session
-                session(['api_token' => $response->json('token')]); // Contoh menyimpan token
-
-                // Redirect ke dashboard setelah login berhasil
-                return redirect()->route('dashboard');
-            } else {
-                // Menangani kesalahan jika login gagal
-                return redirect()->back()->withErrors('Login gagal. Periksa email/telepon atau password.');
-            }
-
-=======
             // Cek apakah data ada di tabel `users`
             $user = \App\Models\User::where('email', $request->email)->first();
 
             if (!$user) {
                 return redirect()->back()->withErrors('Email tidak ditemukan.');
             }
->>>>>>> c6efe51c5ae24bc550f217dd1fb39696f1de8917
 
             // Verifikasi password
             if (!Hash::check($request->password, $user->password)) {
