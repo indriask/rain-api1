3223d035404ceb9efb83eebfc266a5f97e8337a7
ffmdn13 <fafasnakes314@gmail.com>
2024-12-10 08:35:42 +0700

    Added named route to api route

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\API\SearchController;
 
 
 
-Route::post('/signin', [AccountController::class, 'signin']);
-Route::post('/signup', [AccountController::class, 'signup']);
-Route::post('/signout', [AccountController::class, 'signout']);
+Route::post('/signin', [AccountController::class, 'signin'])->name('api-validate-signin');
+Route::post('/signup', [AccountController::class, 'signup'])->name('api-validate-signup');
+Route::post('/signout', [AccountController::class, 'signout'])->name('api-validate-signout');
 Route::post('/search/company', [SearchController::class, 'searchPerusahaan']);
