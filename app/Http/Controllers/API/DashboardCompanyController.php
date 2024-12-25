ff5237627d9da923e1691ad6a59ae6c7aaea26ca
ria <indriaquysi@gmail.com>
2024-12-25 20:21:43 +0700

    test

--- app/Http/Controllers/API/DashboardCompanyController.php
+++ app/Http/Controllers/API/DashboardCompanyController.php
@@ -7,13 +7,6 @@ use Illuminate\Http\Request;
 
 class DashboardCompanyController extends Controller
 {
-    /**
-     * Method untuk mem-proses logika tambah lowongan
-     * kak ini yg rain lama
-     * kalo folder rain yang dari saya, aman kak ?
-     * ga tau sih, nah itu pindahin deh kak keluarin dr folder magang itu
-     * okay kak
-     */
     public function tambahLowongan(Request $request) {}
 
     /**
