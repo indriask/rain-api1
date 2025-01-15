b7a73bfca554825be63b971ba8e4368e2b260220
ffmdn13 <fafasnakes314@gmail.com>
2025-01-15 10:41:20 +0700

    Fix bug

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,5 +23,6 @@ class DatabaseSeeder extends Seeder
         $this->call(StudentSeeder::class);
         $this->call(CompanySeeder::class);
         $this->call(AdminSeeder::class);
+        // $this->call(VacancySeeder::class);
     }
 }
